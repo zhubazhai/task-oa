087e356192cc2807195c4b07e7c7d45ef7cec1db
ex_lingfeng.meng <ex_lingfeng.meng@tcl.com>
2023-06-18 19:51:27 +0800

    feat: Foundation layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Button, Tag, Table } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    tableData: []
+  };
+  columns = [];
+  render() {
+    let { tableData } = this.state;
+    return (
+      <div className="App">
+        <h2 className="App-header">
+          <span className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <span> OA 任务管理系统</span>
+          </span>
+          <Button size="small">新增任务</Button>
+        </h2>
+        <div className="App-body">
+          <div className="tag-box">
+            <Tag color="#282c34">全部</Tag>
+            <Tag>已完成</Tag>
+            <Tag>未完成</Tag>
+          </div>
+          <Table
+            dataSource={tableData}
+            columns={this.columns}
+            loading={false}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
